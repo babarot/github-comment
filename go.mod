2587dd8825e65f1a7f5e39a15d831deca43270ef
b4b4r07 <b4b4r07@gmail.com>
2020-02-12 19:42:07 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/b4b4r07/github-comment
+
+go 1.13
+
+require (
+	github.com/b4b4r07/go-cli-log v0.0.0-20200124120248-8fac4d71de01
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/jessevdk/go-flags v1.4.0
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	clilog "github.com/b4b4r07/go-cli-log"
+	"github.com/google/go-github/github"
+	"github.com/jessevdk/go-flags"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+// These variables are set in build step
+var (
+	Version  = "unset"
+	Revision = "unset"
+)
+
+// CLI represents this application itself
+type CLI struct {
+	Option Option
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+// Option represents application options
+type Option struct {
+	Number     int    `long:"number" description:"Number"`
+	Repository string `long:"repository" description:"user/repo"`
+	Body       string `long:"body" description:"Body"`
+	Version    bool   `long:"version" description:"Show version"`
+}
+
+func main() {
+	os.Exit(run(os.Args[1:]))
+}
+
+func run(args []string) int {
+	clilog.Env = "CLI_LOG"
+	clilog.SetOutput()
+	defer log.Printf("[INFO] finish main function")
+
+	log.Printf("[INFO] Version: %s (%s)", Version, Revision)
+	log.Printf("[INFO] Args: %#v", args)
+
+	var opt Option
+	args, err := flags.ParseArgs(&opt, args)
+	if err != nil {
+		return 2
+	}
+
+	cli := CLI{
+		Option: opt,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	if err := cli.Run(args); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+
+	return 0
+}
+
+func (c *CLI) Run(args []string) error {
+	token := os.Getenv("GITHUB_TOKEN")
+	if len(token) == 0 {
+		return errors.New("GITHUB_TOKEN is missing")
+	}
+
+	// Construct github HTTP client
+	ts := oauth2.StaticTokenSource(&oauth2.Token{
+		AccessToken: token,
+	})
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
+
+	slugs := strings.Split(c.Option.Repository, "/")
+	num := c.Option.Number
+	body := c.Option.Body
+
+	// Check there are no same comments
+	comments, _, err := client.Issues.ListComments(context.Background(), slugs[0], slugs[1], num, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, comment := range comments {
+		if comment.GetBody() == body {
+			log.Printf("[INFO] comment %q was already posted, skip it", body)
+			continue
+		}
+	}
+
+	if _, _, err := client.Issues.CreateComment(context.Background(), slugs[0], slugs[1], num, &github.IssueComment{
+		Body: &body,
+	}); err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Successfully created a comment!")
+	return nil
+}
